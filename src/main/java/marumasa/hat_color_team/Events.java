73993c9a5b9c8f63473e7a2600e9d7fe475193ac
malken21 <Malken_Marumasa@outlook.jp>
2023-03-30 02:40:32 +0900

    optimization

--- src/main/java/marumasa/hat_color_team/Events.java
+++ src/main/java/marumasa/hat_color_team/Events.java
@@ -1,7 +1,6 @@
 package marumasa.hat_color_team;
 
 import org.bukkit.Color;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,16 +11,11 @@ import org.bukkit.inventory.meta.LeatherArmorMeta;
 public class Events implements Listener {
     @EventHandler
     public void onDamage(EntityDamageByEntityEvent event) {
-        Entity entity = event.getEntity();
-
-
-        if (entity instanceof Player player) {
-            Color color = getColor(player);
+        if (event.getEntity() instanceof Player player) {
+            final Color color = getColor(player);
             if (event.getDamager() instanceof Player player2) {
-                Color color2 = getColor(player2);
-                if (color.equals(color2)) {
-                    event.setCancelled(true);
-                }
+                final Color color2 = getColor(player2);
+                if (color.equals(color2)) event.setCancelled(true);
             }
         }
     }
@@ -29,9 +23,8 @@ public class Events implements Listener {
     public Color getColor(Player player) {
         ItemStack itemStack = player.getInventory().getHelmet();
         if (itemStack == null) return null;
-        if (itemStack.getItemMeta() instanceof LeatherArmorMeta leatherArmorMeta) {
+        if (itemStack.getItemMeta() instanceof LeatherArmorMeta leatherArmorMeta)
             return leatherArmorMeta.getColor();
-        }
         return null;
     }
 }
