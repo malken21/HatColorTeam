0b76753ffd77c0b168ec7cdc50abe789a2080ac2
malken21 <Malken_Marumasa@outlook.jp>
2023-04-01 02:10:53 +0900

    Update Events.java

--- src/main/java/marumasa/hat_color_team/Events.java
+++ src/main/java/marumasa/hat_color_team/Events.java
@@ -1,7 +1,9 @@
 package marumasa.hat_color_team;
 
 import org.bukkit.Color;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -13,18 +15,29 @@ public class Events implements Listener {
     public void onDamage(EntityDamageByEntityEvent event) {
         if (event.getEntity() instanceof Player player) {
             final Color color = getColor(player);
-            if (event.getDamager() instanceof Player player2) {
-                final Color color2 = getColor(player2);
-                if (color.equals(color2)) event.setCancelled(true);
-            }
+            if (color == null) return;
+            final Entity attacker = event.getDamager();
+
+            if (attacker instanceof Player player2)
+                event.setCancelled(isSameColor(player2, color));
+
+            else if (attacker instanceof Projectile projectile)
+                if (projectile.getShooter() instanceof Player player2)
+                    event.setCancelled(isSameColor(player2, color));
         }
     }
 
-    public Color getColor(Player player) {
+    private static Color getColor(Player player) {
         ItemStack itemStack = player.getInventory().getHelmet();
         if (itemStack == null) return null;
         if (itemStack.getItemMeta() instanceof LeatherArmorMeta leatherArmorMeta)
             return leatherArmorMeta.getColor();
         return null;
+    }
+
+    private static boolean isSameColor(Player player, Color color) {
+        final Color color2 = getColor(player);
+        if (color2 == null) return false;
+        return color.equals(color2);
     }
 }
